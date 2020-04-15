1f345dd070c2791586bfbd8391d3e6e6e2cbf727
Dholex <arman1717@hotmail.com>
2020-04-15 10:06:52 -0500

    Update to skeleton template

---
+++ include.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+	
+	MOD_GUILD_Z_SYSTEM_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
+	
+	source $MOD_GUILD_Z_SYSTEM_ROOT"/conf/conf.sh.dist"
+	
+	if [ -f $MOD_GUILD_Z_SYSTEM_ROOT"/conf/conf.sh" ]; then
+	    source $MOD_GUILD_Z_SYSTEM_ROOT"/conf/conf.sh"
+	fi
